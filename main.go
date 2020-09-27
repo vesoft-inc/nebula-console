f3315bbca82b0fc36651fa47922323b58ba03779
jievince <38901892+jievince@users.noreply.github.com>
2020-09-27 16:50:00 +0800

    don't print username and password when connect error

--- main.go
+++ main.go
@@ -191,7 +191,7 @@ func main() {
 	}
 
 	if err = client.Connect(*username, *password); err != nil {
-		log.Panicf("Fail to connect server, username: %s, password: %s, %s", *username, *password, err.Error())
+		log.Panicf("Fail to connect server, ", err.Error())
 	}
 
 	welcome(interactive)
