aba7117500deb07b8651ba2c39131292f13e627f
Yichen Wang <18348405+Aiee@users.noreply.github.com>
2021-05-10 15:12:31 +0800

    update module tag (#101)

--- printer/dataset_printer.go
+++ printer/dataset_printer.go
@@ -12,7 +12,7 @@ import (
 	"strings"
 
 	"github.com/jedib0t/go-pretty/v6/table"
-	nebula "github.com/vesoft-inc/nebula-go"
+	nebula "github.com/vesoft-inc/nebula-go/v2"
 )
 
 type DataSetPrinter struct {
